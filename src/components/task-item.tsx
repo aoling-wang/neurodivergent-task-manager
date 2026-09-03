d510d17e419c4664b4d2002d6a85171521227149
Kevin Wang <aoling.wang@gmail.com>
2026-09-02 21:04:21 -0400

    Add styles to task item component

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -2,19 +2,51 @@ import { Task } from '@/app/(tabs)/index';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { FontAwesome } from '@react-native-vector-icons/fontawesome';
-import { Pressable, StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+const complexityTagColors: Record<string, string> = {
+    simple: '#569139', // dark green
+    moderate: '#d0c060', // dark khaki
+    complex: '#d06060', // dark coral
+};
 
 export default function TaskItem({ task, toggleTask }: { task: Task; toggleTask: (id: number) => void }) {
     return (
-        <Pressable onPress={() => toggleTask(task.id)}>
-            <ThemedView style={styles.taskItem}>
-                <FontAwesome name={task.completed ? 'check-square' : 'square-o'} size={24} color="black" />
-                <ThemedText style={styles.taskText}>{task.title}</ThemedText>
+        <TouchableOpacity onPress={() => toggleTask(task.id)}>
+            <ThemedView style={styles.taskContainer}>
+                {task.completed ? 
+                <FontAwesome name="check-square" size={22} color="black" style={styles.icon}/> : 
+                <FontAwesome name="square-o" size={24} color="black" style={styles.icon}/>}
+                <ThemedText style={styles.title}>{task.title}</ThemedText>
+                <ThemedText style={styles.tag}>{task.estimatedMinutes} min</ThemedText>
             </ThemedView>
-        </Pressable>
+        </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
+    taskContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+        paddingVertical: 2,
+        backgroundColor: 'transparent',
+    },
 
+    icon: {
+        backgroundColor: 'white',
+    },
+
+    title: {
+        fontWeight: '400',
+    },
+
+    tag: {
+        marginLeft: 'auto',
+        fontSize: 12,
+        paddingHorizontal: 8,
+        paddingVertical: 0,
+        borderRadius: 12,
+        backgroundColor: '#e0e0e0',
+    },
 })
