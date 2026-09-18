7b9a45be285a8fc106da998520217706664f93f7
Kevin Wang <aoling.wang@gmail.com>
2026-09-18 01:36:35 -0400

    Move color scheme to isolated file

---
+++ src/colors/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+    'urgent': '#f43f5e',
+    'simple': '#2DD4BF',
+    'moderate': '#F59E0B',
+    'complex': '#8B5CF6',
+};
+
+export const opacityLevels = {
+    baseline: {
+        'background': '1a',
+        'border': '4d',
+    },
+    dim: {
+        'background': '1a',
+        'border': '4d',
+    },
+}
