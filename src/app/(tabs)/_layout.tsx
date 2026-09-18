b3f5051dd311fd1e8a1c46919da00741c823b55b
Kevin Wang <aoling.wang@gmail.com>
2026-09-18 01:56:46 -0400

    Configure the load-in screen

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'white', tabBarInactiveTintColor: 'grey', tabBarStyle: { backgroundColor: '#000000' } }}>
+    <Tabs initialRouteName="index" screenOptions={{ tabBarActiveTintColor: 'white', tabBarInactiveTintColor: 'grey', tabBarStyle: { backgroundColor: '#000000' } }}>
       <Tabs.Screen
         name="weekly-tasks"
         options={{
